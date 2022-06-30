d9ab43e5e446f40747044fc556353fe7d01ddc8a
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-06-30 21:08:41 +0300

    using .map() and calling the data in map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import Cards from './Cards';
-
+import data from './data';
 function App() {
+
+  const cards = data.map(item => {
+    return (
+      <Cards 
+        img = {item.img}
+        title = {item.title}
+        date = {item.title}
+        view = {item.feedback.view}
+        like = {item.feedback.like}
+        comment = {item.feedback.comment}
+      />
+    )
+  })
   return (
     <div className="container">
-      <Cards />
+      {cards}
     </div>
   );
 }
