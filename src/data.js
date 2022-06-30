269497c49924e691df4270b6b6679b7f49973d90
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-06-30 20:39:32 +0300

    adding file card data

---
+++ src/data.js
@@ -0,0 +1,49 @@
+export default [
+    {
+        id: 1,
+        img: './img/portfolio-1',
+        title: 'Online Education',
+        date: 'Jul 30, 2020',
+        feedback : {
+            view: '1,450',
+            like: 120,
+            comment: 65
+        }
+    },
+
+    {
+        id: 2,
+        img: './img/portfolio-2',
+        title: 'Dark Portfolio',
+        date: 'Jul 25, 2021',
+        feedback : {
+            view: '1,250',
+            like: 220,
+            comment: 131
+        }
+    },
+
+    {
+        id: 3,
+        img: './img/portfolio-3',
+        title: 'Mai Website',
+        date: 'Jan 15, 2021',
+        feedback : {
+            view: '3,450',
+            like: 250,
+            comment: 200
+        }
+    },
+
+    {
+        id: 4,
+        img: './img/portfolio-4',
+        title: 'Dashboard',
+        date: 'April 5, 2022',
+        feedback : {
+            view: '1,950',
+            like: 150,
+            comment: 100
+        }
+    }
+]
